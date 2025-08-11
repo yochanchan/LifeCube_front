ea1fa763327c2b3e2bb94fc114189881bdf9a871
yochanchan <arai0201@gmail.com>
2025-08-11 16:06:03 +0900

    ボタンの色変えた

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -181,7 +181,7 @@ export default function Home() {
           </Link>
           <Link href="/album">
             <button
-              className="bg-grey-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+              className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded"
             >
               album（使わない）
             </button>
